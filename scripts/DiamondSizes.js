8e435c6ead94785633101b39a45336e588ffe151
Ashton Monzi <anmonzi@gmail.com>
2021-04-27 13:13:03 -0500

    fixed initial bugs

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -1,21 +1,21 @@
 import { getSizes } from "./database.js"
 
-const metals = getSizes()
+const sizes = getSizes()
 
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.name === "size") {
-            window.alert(``)
-        }
-    }
-)
+// document.addEventListener(
+//     "change",
+//     (event) => {
+//         if (event.target.name === "size") {
+//             window.alert(``)
+//         }
+//     }
+// )
 
 export const DiamondSizes = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItems = metals.map(size => {
+    const listItems = sizes.map(size => {
         return `<li>
             <input type="radio" name="size" value="${size.id}" /> ${size.carets}
         </li>`
